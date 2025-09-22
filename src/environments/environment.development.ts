3fdef0fee9dc7e8ef12b6ce484b47c0957d5566f
TrEv0rRrRr <valentinosolischang@gmail.com>
2025-09-22 09:23:32 -0500

    chore: add env variables

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+
+  serverBaseUrl: 'https://api.artic.edu/api/v1/',
+  exhibitionsEndpointPath: 'exhibitions',
+  exhibitionFields: 'fields=id,title,short_description,status,web_url,image_url,gallery'
+};
