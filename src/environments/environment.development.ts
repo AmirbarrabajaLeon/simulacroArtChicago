a2c7c08bc84214965af91207b2804ca5ae01ce5f
AmirbarrabajaLeon <amircasan@gmail.com>
2025-09-25 15:32:05 -0500

    chore(env): updated exhibitionParams structure

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,5 +3,8 @@ export const environment = {
 
   serverBaseUrl: 'https://api.artic.edu/api/v1/',
   exhibitionsEndpointPath: 'exhibitions',
-  exhibitionFields: 'fields=id,title,short_description,status,web_url,image_url,gallery'
+  exhibitionParams: {
+    fields: ['id', 'title', 'short_description', 'status', 'web_url', 'image_url', 'gallery_title', 'aic_start_at', 'aic_end_at'],
+    limit: 25
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,10 @@
 export const environment = {
-  production: false,
+  production: true,
 
   serverBaseUrl: 'https://api.artic.edu/api/v1/',
   exhibitionsEndpointPath: 'exhibitions',
-  exhibitionFields: 'fields=id,title,short_description,status,web_url,image_url,gallery'
+  exhibitionParams: {
+    fields: ['id', 'title', 'short_description', 'status', 'web_url', 'image_url', 'gallery_title', 'aic_start_at', 'aic_end_at'],
+    limit: 25
+  }
 };
