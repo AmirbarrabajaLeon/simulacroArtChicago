3d4b968b2ff5bca36ffb81686d408858edd22661
AmirbarrabajaLeon <amircasan@gmail.com>
2025-09-25 18:47:34 -0500

    feat(language-switcher): implemented first version of the language switcher
    - Implemented the layout using button-toggle component

---
+++ src/app/shared/presentation/components/language-switcher/language-switcher.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MatButtonToggle, MatButtonToggleGroup} from '@angular/material/button-toggle';
+
+@Component({
+  selector: 'app-language-switcher',
+  imports: [
+    MatButtonToggleGroup,
+    MatButtonToggle
+  ],
+  templateUrl: './language-switcher.html',
+  styleUrl: './language-switcher.css'
+})
+export class LanguageSwitcher {
+
+}
