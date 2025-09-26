f64191bc3da8c5f1336fc805572f5f622d2069ab
AmirbarrabajaLeon <amircasan@gmail.com>
2025-09-25 19:17:49 -0500

    feat(app): integrated toolbar, footer content, and exhibitions view components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Toolbar} from './shared/presentation/components/toolbar/toolbar';
+import {FooterContent} from './shared/presentation/components/footer-content/footer-content';
+import {ExhibitionsView} from './exhibitions/presentation/views/exhibitions-view/exhibitions-view';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Toolbar, FooterContent, ExhibitionsView],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
