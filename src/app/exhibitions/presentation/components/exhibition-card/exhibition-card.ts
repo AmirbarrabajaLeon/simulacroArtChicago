04412367fc556080e242e56d876e35388b4a7c27
AmirbarrabajaLeon <amircasan@gmail.com>
2025-09-26 02:32:08 -0500

    fix(exhibition-card): remove unnecessary inject import

--- src/app/exhibitions/presentation/components/exhibition-card/exhibition-card.ts
+++ src/app/exhibitions/presentation/components/exhibition-card/exhibition-card.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 import { Exhibition } from '../../../domain/model/exhibition.entity';
 
