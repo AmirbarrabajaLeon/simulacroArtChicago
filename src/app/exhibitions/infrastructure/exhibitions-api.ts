2bf31f87eafd7a9307f5243f0f1e234971843572
AmirbarrabajaLeon <amircasan@gmail.com>
2025-09-25 23:49:37 -0500

    feat(exhibitions-api): implement ExhibitionsApi service for fetching exhibition data

---
+++ src/app/exhibitions/infrastructure/exhibitions-api.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import {HttpClient, HttpParams} from '@angular/common/http';
+
+import {map, Observable} from 'rxjs';
+
+import { Exhibition } from '../domain/model/exhibition.entity';
+import { ExhibitionsResponse } from './exhibitions-response';
+import { ExhibitionAssembler } from './exhibition-assembler';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ExhibitionsApi {
+
+  private baseUrl = environment.serverBaseUrl;
+  private exhibitionsEndpoint = environment.exhibitionsEndpointPath;
+  private params = new HttpParams()
+    .set('fields', environment.exhibitionParams.fields.join(','))
+    .set('limit', environment.exhibitionParams.limit);
+
+  constructor(private http: HttpClient) {}
+
+  getExhibitions(): Observable<Exhibition[]> {
+    return this.http.get<ExhibitionsResponse>(`${this.baseUrl}${this.exhibitionsEndpoint}?${this.params.toString()}`).pipe(
+      map(response => ExhibitionAssembler.toEntitiesFromResponse(response))
+    );
+  }
+}
