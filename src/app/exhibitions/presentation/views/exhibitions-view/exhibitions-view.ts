320a39ec69d174c8a1fd292a5d6b762168ddb291
AmirbarrabajaLeon <amircasan@gmail.com>
2025-09-26 02:25:28 -0500

    feat(exhibitions-view): implement exhibitions view component with layout and data loading

--- src/app/exhibitions/presentation/views/exhibitions-view/exhibitions-view.ts
+++ src/app/exhibitions/presentation/views/exhibitions-view/exhibitions-view.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject} from '@angular/core';
+
+import { ExhibitionList } from '../../components/exhibition-list/exhibition-list';
+import {ExhibitionsStore} from '../../../application/exhibitions.store';
 
 @Component({
   selector: 'app-exhibitions-view',
-  imports: [],
+  imports: [
+    ExhibitionList
+  ],
   templateUrl: './exhibitions-view.html',
   styleUrl: './exhibitions-view.css'
 })
-export class ExhibitionsView {
 
+export class ExhibitionsView implements OnInit {
+
+  protected store = inject(ExhibitionsStore);
+  protected readonly exhibitions= this.store.exhibitions;
+
+  ngOnInit(): void {
+    this.store.loadExhibitions();
+    console.log(this.store.exhibitions());
+  }
 }
