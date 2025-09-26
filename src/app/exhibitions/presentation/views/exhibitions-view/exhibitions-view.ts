97d16b7c81b9123b8c678df4ce0ae88730407022
AmirbarrabajaLeon <amircasan@gmail.com>
2025-09-25 19:17:20 -0500

    feat(exhibitions-view): added layout exhibitions view component

---
+++ src/app/exhibitions/presentation/views/exhibitions-view/exhibitions-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exhibitions-view',
+  imports: [],
+  templateUrl: './exhibitions-view.html',
+  styleUrl: './exhibitions-view.css'
+})
+export class ExhibitionsView {
+
+}
